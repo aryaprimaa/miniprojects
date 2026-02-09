3526de41d8f2ccd99eb7444837fab223dd7d373c
Arya Prima <aryaprima991@gmail.com>
2026-02-09 18:24:10 +0700

    report query

--- order-service/src/main/java/com/example/orderservice/repository/ReportRepository.java
+++ order-service/src/main/java/com/example/orderservice/repository/ReportRepository.java
@@ -12,14 +12,11 @@ import java.util.List;
 public interface ReportRepository extends JpaRepository<Order, Long> {
 
     @Query(value = """
-        SELECT p.category AS category,
-               SUM(oi.quantity * oi.price) AS totalRevenue
+        SELECT p.category AS category, SUM(oi.quantity * oi.price) AS totalRevenue
         FROM orders o
         JOIN order_items oi ON o.id = oi.order_id
         JOIN products p ON p.id = oi.product_id
         WHERE o.status = 'COMPLETED'
-        GROUP BY p.category
-        ORDER BY totalRevenue DESC
-    """, nativeQuery = true)
+        GROUP BY p.category ORDER BY totalRevenue DESC """, nativeQuery = true)
     List<RevenueByCategoryDTO> getRevenueByCategory();
 }
